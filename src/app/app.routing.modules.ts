7a226bd5dd9b6ea03f007423db14fbd65555fe21
Americo Bocchini <americo.bocchini@lambda3.com.br>
2021-11-24 17:29:38 -0300

    roteamento e busca

--- src/app/app.routing.modules.ts
+++ src/app/app.routing.modules.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListResolve } from './photos/photo-list/photo.list.resolve';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path: 'user/:userName', component: PhotoListComponent
+    path: 'user/:userName', component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolve
+    }
   },
   {
     path: 'p/add', component: PhotoFormComponent
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,3 +1,4 @@
+import { FilterByDescrition } from './photo-list/photos/filter.bt.descrition.pipe';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -5,12 +6,15 @@ import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotoListComponent,
-    PhotoFormComponent
+    PhotoFormComponent,
+    PhotosComponent,
+    FilterByDescrition
   ],
   imports: [
     HttpClientModule,
