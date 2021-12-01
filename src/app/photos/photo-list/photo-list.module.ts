02ef155d67d59d2c06eb988b2c2bb8b08d30a68b
Americo Bocchini <americo.bocchini@lambda3.com.br>
2021-12-01 16:28:49 -0300

    Component search

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,20 +1,23 @@
-import { CardModule } from './../../shared/components/card/card.module';
-import { PhotoModule } from './../photo/photo.module';
 import { FilterByDescrition } from './photos/filter.bt.descrition.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
-import { PhotosComponent } from './photos/photos.component';
 import { PhotoListComponent } from './photo-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SearchComponent } from './search/search.component';
+import { CardModule } from './../../shared/components/card/card.module';
+import { PhotoModule } from './../photo/photo.module';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
     PhotoListComponent,
     PhotosComponent,
     LoadButtonComponent,
-    FilterByDescrition
+    FilterByDescrition,
+    SearchComponent
   ],
-  imports: [ CommonModule, PhotoModule, CardModule ]
+  imports: [ CommonModule, PhotoModule, CardModule]
 })
 export class PhotoListModule{
 
